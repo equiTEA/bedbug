c837b46705248734e674e06be285cb40b03a2532
Jeff <jefferydreynolds@gmail.com>
2022-10-19 16:03:12 -0600

    more cookie logs

--- apps/keystone/config/auth.ts
+++ apps/keystone/config/auth.ts
@@ -20,9 +20,28 @@ const { withAuth } = createAuth({
 let sessionSecret = env.COOKIE_SECRET
 let sessionMaxAge = 60 * 60 * 24 * 30 // 30 days
 
+const domainValue =
+  process.env.NODE_ENV === 'production'
+    ? // Get the domain by removing the protocol and the trailing slash path
+      (env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string)
+        .split(
+          `http${
+            env.NEXT_PUBLIC_GRAPHQL_ENDPOINT.startsWith('https://') ? 's' : ''
+          }://`,
+        )[1]
+        .split('/')[0]
+    : 'localhost'
+
+console.log({
+  domainValue,
+  sameSiteValue: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  secureValue: process.env.NODE_ENV === 'production',
+})
+
 const session = statelessSessions({
   maxAge: sessionMaxAge,
   secret: sessionSecret,
+  secure: process.env.NODE_ENV === 'production',
   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   domain:
     process.env.NODE_ENV === 'production'
